6fa5a55d2cf9a0d5b90dd69203f2bb08dde761b1
gdserafini <gdinizserafini@gmail.com>
2024-10-24 22:27:15 -0300

    updated -> linear algebra

--- src/extra-linear-algebra/code.py
+++ src/extra-linear-algebra/code.py
@@ -1,16 +1,20 @@
 class Vector:
-  def __init__(self, 
-              x1: int, y1: int, z1: int 
-              x2: int, y2: int, z2: int) -> None:
+  def get(self) -> tuple: pass
+
+class Vector2D(Vector):
+  def __init__(self,
+               x1: int, x2: int,
+               y1: int, y2: int) -> None:
     self._x1 = x1
     self._y1 = y1
-    self._z1 = z1
     self._x2 = x2
     self._y2 = y2
-    self._z2 = z2
 
-  def get() -> tuple:
-    return (
-      self._x1, self._y1, self._z1
-      self._x2, self._y2, self._z2
-    )
+    def get(self) -> tuple:
+      return self._x1, self._y1, self._x2, self._y2
+
+class VectorOperator:
+  def add(self, v: Vector, w: Vector) -> Vector: pass
+
+class Vector2DOperator(VectorOperator):
+  pass
