d2a19166e9d59cdea7885dbb381081350889c054
gdserafini <gdinizserafini@gmail.com>
2024-11-08 09:17:35 -0300

    update linear algebra code

--- src/extra-linear-algebra/code.py
+++ src/extra-linear-algebra/code.py
@@ -2,27 +2,34 @@ class Vector:
   """
   This class represents a tridimensional vector.
   """
-  def __init__(self, x, y, z):
+  def __init__(self, x, y, z) -> None:
     self.x = x
     self.y = y
     self.z = z
 
-  def _validate(object: Any, object_class: Any) -> None
-    if not object or not oclass: 
-      raise ValueError("Invalid parans.")
-    if not isinstance(object, object_class) 
-      raise ValueError("Invalid data type.")
-
-  def __add__(self: Vector, other: Vector) -> Vector:
-    self._validate(self, Vector)
-    self._validate(other, Vector)
+  def __add__(self, other):
+    Validator(self).validate()
+    Validator(other).validate()
     return Vector(
       self.x + other.x, self.y + other.y, self.z + other.z
     )
 
-  def __sub__(self: Vector, other: Vector) -> Vector:
-    self._validate(self, Vector)
-    self._validate(other, Vector)
+  def __sub__(self, other):
+    Validator(self).validate()
+    Validator(other).validate()
     return Vector(
       self.x - other.x, self.y - other.y, self.z - other.z
     )
+
+class Validator:
+  _vector: Vector
+
+  def __init__(self, vector: Vector) -> None:
+    self._vector = vector
+
+  @staticmethod
+  def validate(_vector=None) -> None:
+    if not _vector:
+      raise ValueError("Invalid params.")
+    if not isinstance(_vector, Vector):
+      raise ValueError("Invalid data type.")
