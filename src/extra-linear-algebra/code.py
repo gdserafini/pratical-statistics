da498607b09ec09a019b46775ce3563e97ed28c8
gdserafini <gdinizserafini@gmail.com>
2024-11-10 15:28:34 -0300

    updated code

--- src/extra-linear-algebra/code.py
+++ src/extra-linear-algebra/code.py
@@ -2,11 +2,11 @@ class Vector:
   """
   This class represents a tridimensional vector.
   """
-  def __init__(self, x, y, z) -> None:
+  def __init__(self, x: int = 0, y: int = 0, z: int = 0) -> None:
     self.x = x
     self.y = y
     self.z = z
-    _validator = _Validator()
+    self._validator = _Validator()
 
   def __add__(self, other):
     self._validator.validate(self)
@@ -22,6 +22,9 @@ class Vector:
       self.x - other.x, self.y - other.y, self.z - other.z
     )
 
+  def __str__(self) -> str:
+    return f'[{self.x}, {self.y}, {self.z}]'
+
 class _Validator:
   @staticmethod
   def validate(_vector: Vector) -> None:
@@ -29,3 +32,6 @@ class _Validator:
       raise ValueError("Invalid params.")
     if not isinstance(_vector, Vector):
       raise ValueError("Invalid data type.")
+    if (_vector.x < 0 or _vector.y < 0 or _vector.z < 0) or\
+      (type(_vector.x) != int or type(_vector.y) != int or type(_vector.z) != int):
+      raise ValueError("Invalid vector values.")
