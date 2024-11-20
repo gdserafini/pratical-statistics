3d4f53182207d2cf7da4911a50e7d3e1961793c0
gdserafini <gdinizserafini@gmail.com>
2024-11-20 20:49:41 -0300

    updated -> la code

--- src/extra-linear-algebra/code.py
+++ src/extra-linear-algebra/code.py
@@ -4,7 +4,6 @@ class Vector:
   """
   def __init__(self, x: float = 0, y: float = 0, z: float = 0) -> None:
     self._validator = _Validator()
-    self._validator.validate_types([x, y, z], float)
     self.x = x
     self.y = y
     self.z = z
@@ -26,11 +25,10 @@ class Vector:
   def __str__(self) -> str:
     return f'[{self.x}, {self.y}, {self.z}]'
 
-  def get(self) -> tuple: return (self.x, self.y, self.z)
+  def get(self) -> tuple: return self.x, self.y, self.z
 
   def set(self, x: float = None, 
           y: float = None, z: float = None) -> None:
-    _validator.validate_types([x, y, z], float)
     self.x = x if x else self.x
     self.y = x if y else self.y
     self.z = x if z else self.z
@@ -41,20 +39,17 @@ class Matrix:
   This class represents a mutable matrix.
   """
   def __init__(self, matrix: list[list] = None, i: int = 0, j: int = 0) -> None:
-    self._validator = Validator()
-    self.validator.validate_types(matrix, float)
     self.i = i
     self.j = j
     self.matrix = matrix if matrix else\
     [[
-      0 for _ in range(self._j)]
-      for _ in range(self._i)
+      0 for _ in range(self.j)]
+      for _ in range(self.i)
     ]
 
   def get(self) -> list[list]: return self.matrix
 
   def set(self, matrix: list[list] = None) -> None:
-    self.validator.validate_types(matrix, float)
     self.matrix = matrix if matrix else self.matrix
 
 
@@ -74,17 +69,3 @@ class _Validator:
       raise ValueError("Invalid data type.")
     if matrix.i < 0 or matrix.j < 0 or not matrix.matrix:
       raise ValueError("Invalid matrix")
-
-  @staticmethod
-  def validate_types(values: list[Any], type: Any) -> None:
-    if not values or not type: 
-      raise ValueError('Invalid params.')
-    if isinstance(values, list[list]):
-      for line in values:
-        for value in lines:
-          if not value or not isinstance(value, type):
-            raise ValueError('Invalid data type.')
-    else:
-      for values in values:
-        if not value or not isinstance(value, type):
-          raise ValueError('Invalid data type.')
