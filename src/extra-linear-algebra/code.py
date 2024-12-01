f2510109385f2b70faac417d022550dbd0745e0a
gdserafini <gdinizserafini@gmail.com>
2024-12-01 12:26:25 -0300

    update -> add sum of list[Vector]

--- src/extra-linear-algebra/code.py
+++ src/extra-linear-algebra/code.py
@@ -7,21 +7,21 @@ class Vector:
 
     def get(self): return self._values
 
-    def _validate_vector(self, vector: Any) -> None:
-        if len(self._values) != len(vector.get()) or\
-                not isinstance(vector, Vector):
-            raise ValueError('Invalid vector')
-
     def __add__(self, other: Any) -> Any:
-        self._validate_vector(other)
         return Vector(
             [xa + xb for xa, xb in zip(self._values, other.get())]
         )
 
     def __sub__(self, other: Any) -> Any:
-        self._validate_vector(other)
         return Vector(
             [xa - xb for xa, xb in zip(self._values, other.get())]
         )
 
     def __str__(self): return str(self._values)
+
+
+def sum_vectors(vectors: list[Vector]) -> Vector:
+    vector_sum = Vector([0,0,0])
+    for vector in vectors:
+        vector_sum = vector_sum + vector
+    return vector_sum
