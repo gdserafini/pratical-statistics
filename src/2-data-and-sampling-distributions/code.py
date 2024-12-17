49df4b8b36688ca5ac94ad3a5f391c199a9c1ab9
Giordano D. Serafini <gdinizserafini@gmail.com>
2024-12-17 17:00:57 -0300

    Update code.py

--- src/2-data-and-sampling-distributions/code.py
+++ src/2-data-and-sampling-distributions/code.py
@@ -8,6 +8,7 @@ def _z_value(data: Any) -> float:
 def sample(
     data: Any, replacement: bool = False,
     error_margin: float = 0.05, confidence_level: float = 0.95,
-    distribution: float = 0.5, increase: float = 0.0
+    distribution: float = 0.5, increase: float = 0.0,
+    stratum_column: str = None, strata: list[str] = None
 ) -> Any:
     pass
