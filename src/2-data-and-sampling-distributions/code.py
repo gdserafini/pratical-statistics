ff2424932130816858ff91e10452180cbf4cd9d7
Giordano D. Serafini <gdinizserafini@gmail.com>
2024-12-17 16:57:06 -0300

    Update code.py

--- src/2-data-and-sampling-distributions/code.py
+++ src/2-data-and-sampling-distributions/code.py
@@ -1,5 +1,13 @@
 from typing import Any
 
 
-def random_sampling(data: Any, size: int, replacement: bool) -> Any:
+def _z_value(data: Any) -> float:
+    pass
+
+
+def sample(
+    data: Any, replacement: bool = False,
+    error_margin: float = 0.05, confidence_level: float = 0.95,
+    distribution: float = 0.5, increase: float = 0.0
+) -> Any:
     pass
